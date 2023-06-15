0fd8dd8335cc1f12dd5fb46b8272279de816f20d
Usuario <Usuario@CPD03-M05>
2023-06-15 14:41:20 -0300

    Adicionar arquivos de projeto.

---
+++ AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
@@ -0,0 +1,63 @@
+﻿using AppBancoDigital.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBancoDigital.Service
+{
+    public class DataServiceCorrentista : DataService
+    {
+        /**
+         * Realiza o login do correntista.
+         */
+        public static async Task<Correntista> LoginAsync(Correntista c)
+        {
+            var json_to_send = JsonConvert.SerializeObject(c);
+
+            string json = await DataService.GetDataFromService(String.Format("/correntista/login?cpf={0}&senha={1}", c.CPF, c.Senha));
+                
+            return JsonConvert.DeserializeObject<Correntista>(json);
+        }
+
+        //Envia um Model em forma de JSON pra insert no banco.
+        public static async Task<Correntista> CadastrarCorrentistas(Correntista c)
+        {
+            var json_to_send = JsonConvert.SerializeObject(c);
+            //Console.WriteLine("_____________________________________________");
+            //Console.WriteLine(json_to_send);
+            //Console.WriteLine("_____________________________________________");
+
+            string json = await DataService.PostDataToService(json_to_send, "/correntista/save");
+
+            return JsonConvert.DeserializeObject<Correntista>(json);
+        }
+    }
+
+    /**
+    
+         //Obtém a lista de correntistas
+    public static async Task<List<Correntista>> GetCorrentistasAsync()
+    {
+        string json = await DataService.GetDataFromService("/correntista");
+
+        List<Correntista> arr_correntista = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+        return arr_correntista;
+    }
+    
+        //Deleta uma pessoa do banco de dados.
+    public static async Task<List<Correntista>> DeleteAsync(int id)
+    {
+        var json_a_enviar = JsonConvert.SerializeObject(id);
+
+        string json = await DataService.PostDataToService(json_a_enviar, "/correntista/delete");
+
+        List<Correntista> arr_correntista = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+        return arr_correntista;
+    }
+
+    */
+}
---
+++ AppBancoDigital/AppBancoDigital/View/Correntista/CadastroCorrentista.xaml.cs
@@ -0,0 +1,148 @@
+﻿using AppBancoDigital.Model;
+using AppBancoDigital.Service;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CadastroCliente : ContentPage
+    {
+        public CadastroCliente()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            VerNaover_senha.Source = ImageSource.FromResource("AppBancoDigital.Assets.eyeOn.png");
+            VerNaover_senha2.Source = ImageSource.FromResource("AppBancoDigital.Assets.eyeOn.png");
+            dtpck_dataNasc.MaximumDate = DateTime.Now;
+            dtpck_dataNasc.MinimumDate = new DateTime(1900, 1, 1);
+        }
+        bool vendo = false;
+        bool vendo2 = false;
+
+        private void VerNaover_senha_Clicked(object sender, EventArgs e)
+        {
+            //FUNÇÃO DE MOSTRAR SENHA -- OK
+            try
+            {
+                if (vendo == false)
+                {
+                    VerNaover_senha.Source = ImageSource.FromResource("AppBancoDigital.Assets.eyeOff.png");
+                    vendo = true;
+                    senha_inserido.IsPassword = false;
+                }
+                else
+                {
+                    VerNaover_senha.Source = ImageSource.FromResource("AppBancoDigital.Assets.eyeOn.png");
+                    vendo = false;
+                    senha_inserido.IsPassword = true;
+                }
+                //VendoNaoVendo(vendo);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro!", ex.Message, "OK");
+            }
+
+        }
+
+        private void VerNaover_senha2_Clicked(object sender, EventArgs e)
+        {
+            //FUNÇÃO DE MOSTRAR SENHA -- OK
+            try
+            {
+                if (vendo2 == false)
+                {
+                    VerNaover_senha2.Source = ImageSource.FromResource("AppBancoDigital.Assets.eyeOff.png");
+                    vendo2 = true;
+                    senhaConfirm_inserido.IsPassword = false;
+                }
+                else
+                {
+                    VerNaover_senha2.Source = ImageSource.FromResource("AppBancoDigital.Assets.eyeOn.png");
+                    vendo2 = false;
+                    senhaConfirm_inserido.IsPassword = true;
+                }
+                //VendoNaoVendo(vendo2);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro!", ex.Message, "OK");
+            }
+        }
+
+        private async void btn_cadastrar_Clicked(object sender, EventArgs e)
+        {
+            //FUNÇÃO DE CADASTRAR CORRENTISTA
+            try
+            {
+                if (senhaConfirm_inserido.Text == senha_inserido.Text)
+                {
+                    string senha = senha_inserido.Text;
+
+                    //ARRUMAR CADASTRO
+                    Correntista c = new Correntista
+                    {
+                        Nome = nome_inserido.Text,
+                        CPF = onlynumber(cpf_inserido.Text),
+                        Data_nasc = dtpck_dataNasc.Date.ToString("yyyy-MM-dd"),
+                        Senha = senha
+                    };
+                    //await DisplayAlert("Sucesso!", "Novo Cliente Cadastrado com Sucesso!", "Ir para Tela Inicial");
+                    //App.Current.MainPage = new NavigationPage(new View.Home());
+
+                    if (c.Id == null)
+                    {
+                        await DataServiceCorrentista.CadastrarCorrentistas(c);
+                        App.DadosCorrentista = c;
+                        await DisplayAlert("Sucesso!", "Novo Cliente Cadastrado com Sucesso!", "Ir para Tela Inicial");
+
+                        //Navegando para a Tela Inicial após cadastrar e definir os dados do Correntista.
+                        //await Navigation.PushAsync(new View.TelaInicial());
+                    }
+                    else
+                        throw new Exception("Ocorreu um erro ao realizar seu cadastro.\nTente Novamente ou realize o Login se já possui uma conta.");
+                }
+                else
+                {
+                    await DisplayAlert("Senhas diferentes!", "Confirme a senha digitada inicialmente", "OK");
+                }
+                //Navigation.ShowPopup(new LoadingPoPup());
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK");
+            }
+        }
+
+        private void btn_voltar_Clicked(object sender, EventArgs e)
+        {
+            //VOLTAR PARA O LOGIN -- OK
+
+            try
+            {
+                App.Current.MainPage = new NavigationPage(new Login());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro!", ex.Message, "OK");
+            }
+        }
+        string onlynumber(string str)
+        {
+            var onlynumber = new Regex(@"[^\d]");
+            return onlynumber.Replace(str, "");
+        }
+
+    }
+}
